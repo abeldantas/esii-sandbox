6d4b60e082d7110dd725616972be8721b822108d
Abel Dantas <dantas.abel@gmail.com>
2025-03-14 21:16:47 +0000

    Configure database

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sandbox.Models;
+
+namespace Sandbox.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    
+    public DbSet<Artist> Artists { get; set; }
+
+    protected override void OnModelCreating( ModelBuilder modelBuilder )
+    {
+        base.OnModelCreating( modelBuilder );
+        
+        // Adicionar configs de BD
+    }
+}
