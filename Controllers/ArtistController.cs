f0c800491c03353f56646724f7fa057a890eb96f
Abel Dantas <dantas.abel@gmail.com>
2025-03-14 22:40:47 +0000

    Add Artist Controller

---
+++ Controllers/ArtistController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sandbox.Data;
+
+namespace Sandbox.Controllers;
+
+[ApiController]
+[Route( "api/[controller]" )]
+public class ArtistController : Controller
+{
+    readonly ApplicationDbContext _context;
+
+    public ArtistController( ApplicationDbContext context )
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetArtists()
+    {
+        var artists = await _context.Artists.ToListAsync();
+        return Ok( artists );
+    }
+}
